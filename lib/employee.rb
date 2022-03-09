707efb44e1f3b61f15eeb3e4b83796d3951cffc3
mrjohnming <john.ngai@hotmail.com>
2022-03-09 03:37:52 +0000

    Add validations

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,20 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+
+  # Employees must always have a first name present.
+  validates :first_name, presence: true
+
+  # Employees must always have a last name present.
+  validates :last_name, presence: true
+
+  # Employees must have an hourly_rate tht is an integer between 40 and 200.
+  validates :hourly_rate,
+    numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 40,
+      less_than_or_equal_to: 200
+    }
+
+  # Employees must always have a store that they belong to.
+  validates :store, presence: true
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,16 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  # Stores must always have a name that is a minimum of 3 characters.
+  validates :name, length: {minimum: 3}
+
+  # Stores have an annual_revenue that is an integer that must be 0 or more.
+  validates :annual_revenue,
+    numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 0
+    }
+
+  # BONUS: A store must carry at least one of the men's or women's apparel.
+  
 end
