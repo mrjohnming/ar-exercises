171a7f626ca5e252c152fc8d1b7536a325a28a57
mrjohnming <john.ngai@hotmail.com>
2022-03-09 00:56:41 +0000

    Add solution

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,8 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+burnaby = Store.create(annual_revenue: 300000)
+richmond = Store.create(annual_revenue: 1260000)
+gastown = Store.create(annual_revenue: 190000)
+
+puts "Store.count = #{Store.count}"
