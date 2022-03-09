f4726daef80a473a727105c8f40cac6cc9eb8d14
mrjohnming <john.ngai@hotmail.com>
2022-03-09 01:45:12 +0000

    Capitalize first letter of each name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -6,7 +6,7 @@ puts "----------"
 
 # Your code goes here ...
 @store1 = Store.find_by(id: 1)
-@store1.name = "toronto"
+@store1.name = "Toronto"
 puts "@store1.name = #{@store1.name}"
 
 @store2 = Store.find_by(id: 2)
