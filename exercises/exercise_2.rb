81e0f4bf81bc73c98dfb3ebaa47d4b4c69d56972
mrjohnming <john.ngai@hotmail.com>
2022-03-09 01:11:57 +0000

    Add solution

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store1.name = "toronto"
+puts "@store1.name = #{@store1.name}"
+
+@store2 = Store.find_by(id: 2)
+puts "@store1.name = #{@store2.name}"
